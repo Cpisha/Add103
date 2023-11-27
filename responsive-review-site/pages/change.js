4bb206604242d83efa962602378c7a31f0412956
Cpisha <142339705+Cpisha@users.noreply.github.com>
2023-11-27 17:08:43 -0600

    Responsive review site project three

---
+++ responsive-review-site/pages/change.js
@@ -0,0 +1,17 @@
+function change01() {
+    document.getElementById("large").src = "../images/option-interior-seats.jpg";
+}
+
+function change02() {
+    document.getElementById("large").src = "../images/option-interior-brown.jpg";
+}
+
+
+function change03() {
+    document.getElementById("large").src = "../images/option-engine.jpg";
+}
+
+function change04() {
+    document.getElementById("large").src = "../images/option-lights.jpg";
+}
+
